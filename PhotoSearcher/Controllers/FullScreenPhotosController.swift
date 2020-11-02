0d303e2be093203bf442870d02c13d37805fa8f2
Mihaela Simion <mihaelasimion2016@gmail.com>
2020-11-02 13:57:14 +0200

    initial setup

---
+++ PhotoSearcher/Controllers/FullScreenPhotosController.swift
@@ -0,0 +1,15 @@
+//
+//  FullScreenPhotosVC.swift
+//  PhotoSearcher
+//
+//  Created by Mihaela Simion on 11/2/20.
+//
+
+import UIKit
+
+class FullScreenPhotosController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
